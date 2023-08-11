f08d53937b0794a152ee9641b1548f90f224e1a7
urinlee0731 <urinlee0731@gmail.com>
2023-08-12 04:30:57 +0900

    renamed:    game/UI/top_ -> game/UI/top.py 	modified:   game/ext/player_camera.py

--- game/ext/player_camera.py
+++ game/ext/player_camera.py
@@ -12,6 +12,7 @@ class Camera(EditorCamera):
         self.position = start_position
         self.max_zoom = 45
         self.zoom_smoothing = 0
+        self.bounce = 350
 
 
     def update(self):
@@ -19,11 +20,11 @@ class Camera(EditorCamera):
         player_x = self.position.x
         player_z = self.position.z
         if player_x < -15 or player_x > 35:
-            self.x += ((int(player_x) * -1)/200)      #x좌표가 30 또는 -30 일떄 반대좌표로 0.2 이동한다 (0.1은 메모리상으로 표현이 어렵기 때문에)
+            self.x += ((int(player_x) * -1)/self.bounce)      #x좌표가 30 또는 -30 일떄 반대좌표로 0.1 이동한다 (하지만 메모리 구조상 0.1은 표현이 어려울것이다)
 
 
         elif player_z < -35 or player_z > 15:
-            self.z += ((int(player_z) * -1)/200)      #z좌표가 30 또는 -30 일떄 반대좌표로 1 이동한다 (0.1은 메모리상으로 표현이 어렵기 때문에)
+            self.z += ((int(player_z) * -1)/self.bounce)      #z좌표가 30 또는 -30 일떄 반대좌표로 0.1 이동한다 (하지만 메모리 구조상 0.1은 표현이 어려울것이다)
 
         else:
             if held_keys["w"]:
