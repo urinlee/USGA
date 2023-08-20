74dbeb39c71b1b59ab573fa1b36c266b32fd3809
urinlee <62046969+urinlee@users.noreply.github.com>
2023-08-20 22:09:47 +0900

    modified:   game/UI/app_window.py 	new file:   sources/item_frame.png 	renamed:    game/src/window.png -> sources/window.png 	modified:   test/once_test.py

--- game/UI/app_window.py
+++ game/UI/app_window.py
@@ -27,7 +27,7 @@ class Shop_window(Entity):
 
         self.win = Entity(parent=camera.ui, 
                              model='quad', 
-                             texture=load_texture("/src/window.png"),
+                             texture=load_texture("/../sources/window.png"),
                              origin=(0,0),  
                              collider='box',
                              scale_x=1.19,
@@ -35,15 +35,38 @@ class Shop_window(Entity):
 
         #cancel = (float(self.scale_x/2), float(self.scale_y/2))
 
-        self.window_name = Text(parent=self.win, text=name, origin=(0, -0.5), x=0, y=0.3, world_scale=0.01)
 
         #self.cancel_container = Entity(parent=self, model="circle", origin=(0.5,0.5), scale_x=0.0, scale_y=0.1, x=cancel[0], y=cancel[1])
+
+        #취소버튼(red_color, left_top, circle)
         self.cancel = Button(parent=self.win, model="circle", color=color.red, world_scale=0.3, x=-0.4145, y=0.376)
-
-
         self.cancel.on_click = self.invisibled
 
-        self.item_container = Entity(parent=self.win, model="quad", world_scale_y=10, world_scale_x=15 , color=color.white)
+
+        self.item_container = Entity(parent=self.win, model="quad", world_scale_y=10, world_scale_x=15, color=color.rgb(0, 0, 0, 0))
+
+        #self.test_pos = Entity(parent=self.item_container, model="quad", x=0.5, y=0.5, scale=0.01, color=color.red)
+
+        self.item_pos = [[n for n in range(ITEM_IN_COUNT)]for i in range(ITEM_LINE)]
+
+        print(self.item_pos)    
+
+        for num, section in enumerate(self.item_pos, 0):
+            self.item_pos[num] = list(map(lambda a: (
+                                                    round(-0.5 + ((a+0.5)/(len(section))), 10) , 
+                                                    round(0.5-((num+0.5)/(len(self.item_pos))), 10)), 
+                                                    section))
+        print(self.item_pos)
+
+        for row in range(len(self.item_pos)):
+            for item in self.item_pos[row]:
+                x = item[0]
+                y = item[1]
+
+                b = Button(parent=self.item_container, model="quad",texture="/../sources/item_frame.png" ,x=x, y=y, world_scale=4.8, origin=(0, 0), color=color.dark_gray, highlight_scale=1.1, highlight_color=color.black)
+                e = Entity(parent=b, model="cube", texture="white_cube", scale=0.5, rotation_x=45, rotation_y=30)
+                print(f"test  {x},{y}")
+        
 
     
 
--- test/once_test.py
+++ test/once_test.py
@@ -3,4 +3,52 @@
 #     max_xp = round(max_xp, -1 * (len(str(max_xp))-2))
 #     print(max_xp)
 
-print(list({"name1":False}.values()))
+# print(list({"name1":False}.values()))
+
+# from ursina import *
+
+# def change_collision(mesh_collider, vertices):
+#     vertices[0] = (0, 0.5, 0)  # 특정 꼭짓점 위치를 변경하여 콜리전 모양 변경
+#     vertices[1] = (0.5, 0.5, 0)
+#     vertices[2] = (0.5, -0.5, 0)
+
+# app = Ursina()
+
+# cube = Entity(model='cube', collider='mesh')
+
+# # MeshCollider 변경 함수를 적용
+# cube.collider.on_update = change_collision
+
+# app.run()
+
+from ursina import *
+
+app = Ursina()
+
+def create_custom_shape():
+    vertices = [
+        (0.5, 0.5),
+        (-0.5, 0.5),
+        (-0.5, -0.5),
+        (0.5, -0.5),
+        (0.25, 0.25),  # 원을 뚫을 위치
+        (-0.25, 0.25),
+        (-0.25, -0.25),
+        (0.25, -0.25)
+    ]
+
+    triangles = [
+        [0, 1, 4],
+        [1, 2, 5],
+        [2, 3, 6],
+        [3, 0, 7]
+    ]
+
+    mesh = Mesh(vertices=vertices, triangles=triangles)
+    return mesh
+
+# 사각형을 만듦
+rectangle = Entity(model=create_custom_shape(), texture='white_cube')
+
+app.run()
+
